ed12c9348d92be5376b04c64b64cd00dda7c5eb9
ItzelVa <itzelochoavallejo@gmail.com>
2024-03-04 11:35:15 -0600

    Step 6: Adding the delete and confirm sections

--- src/UseState.js
+++ src/UseState.js
@@ -8,6 +8,8 @@ export function UseState({name}) {
         value: '',
         error: false,
         loading: false,
+        deleted: false,
+        confirmed: false,
     })
 
     //const [value,setValue] = React.useState('');
@@ -28,6 +30,7 @@ export function UseState({name}) {
                         ...state,
                         loading:false,
                         error: false,
+                        confirmed: true,
                     })
                     //setLoading(false)
                     //setError(false)
@@ -50,36 +53,67 @@ export function UseState({name}) {
         console.log('Terminando el efecto')
     }, [state.loading])
 
-    return (
-        <div>
-            <h2>Eliminar {name}</h2>
-            <p>Por favor, escribe el código de seguridad</p>
-
-            {(state.error && !state.loading) && (
-                <p>Error: El código es incorrecto</p>
-            )}
-
-            {state.loading && (
-                <p>Cargando...</p>
-            )}
-
-            <input 
-                value={state.value}
-                placeholder='Código de seguridad'
-                onChange={(event)=>{
-                    setState({ ...state,
-                                value:event.target.value,})
-                    //setValue(event.target.value)
-                }}/>
-            <button 
-                onClick={()=> {
-                    setState({
-                            ...state,
-                            loading: true})
-                    //setLoading(true)
-                }}>
-                Comprobar
-            </button>
-        </div>
-    )
+    if (!state.deleted && !state.confirmed) {
+        return (
+            <div>
+                <h2>Eliminar {name}</h2>
+                <p>Por favor, escribe el código de seguridad</p>
+    
+                {(state.error && !state.loading) && (
+                    <p>Error: El código es incorrecto</p>
+                )}
+    
+                {state.loading && (
+                    <p>Cargando...</p>
+                )}
+    
+                <input 
+                    value={state.value}
+                    placeholder='Código de seguridad'
+                    onChange={(event)=>{
+                        setState({ ...state,
+                                    value:event.target.value,})
+                        //setValue(event.target.value)
+                    }}/>
+                <button 
+                    onClick={()=> {
+                        setState({
+                                ...state,
+                                loading: true})
+                        //setLoading(true)
+                    }}>
+                    Comprobar
+                </button>
+            </div>
+        )
+    }
+    else if (!!state.confirmed && !state.deleted) {
+        return(
+            <React.Fragment>
+                <p>¿Estás seguro de eliminar?</p>
+                <button onClick={()=>setState({...state,
+                                                deleted:true,})}>
+                    Sí, eliminar
+                </button>
+                <button onClick={()=>setState({...state,
+                                                confirmed:false,
+                                                value:''})}>
+                    No eliminar
+                </button>
+            </React.Fragment>
+        )
+    }
+    else{
+        return(
+            <React.Fragment>
+                <p>Eliminado con éxito</p>
+                <button onClick={()=>setState({...state,
+                                                confirmed:false,
+                                                deleted:false,
+                                                value:'',})}>
+                    Resetear, volver atrás
+                </button>
+            </React.Fragment>
+        )
+    }
 }
